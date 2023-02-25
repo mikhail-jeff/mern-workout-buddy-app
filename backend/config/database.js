7a0e01e2f2716ae421d7b7b99d49ed71af550ab6
jeffmikhail <mikhailcruz78@gmail.com>
2023-02-25 13:20:15 +0800

    mongoose setup and mongodb connection

---
+++ backend/config/database.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+	try {
+		mongoose.set('strictQuery', true);
+		const connect = await mongoose.connect(process.env.MONGO_URI);
+		console.log(`Connected to MongoDB`.brightCyan.underline);
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+module.exports = connectDB;
--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ dotenv.config();
 const app = express();
 
 const workoutRoutes = require('./routes/workouts');
+const connectDB = require('./config/database');
 
 // * middleware
 app.use(express.json());
@@ -15,6 +16,16 @@ app.use(express.urlencoded({ extended: false }));
 app.use('/api/workouts', workoutRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-	console.log(`Server running on http://localhost:${PORT}`.brightCyan.underline);
-});
+
+const startServer = async () => {
+	try {
+		await connectDB();
+		app.listen(PORT, () => {
+			console.log(`Server running on http://localhost:${PORT}`.brightCyan.underline);
+		});
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+startServer();
