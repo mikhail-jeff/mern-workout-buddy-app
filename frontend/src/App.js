3793017c92fe6699ca08888ab0c4ec7dfd5ce232
jeffmikhail <mikhailcruz78@gmail.com>
2023-02-26 10:15:00 +0800

    added react auth context and login/signup forms

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 
 import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 
 function App() {
 	return (
@@ -13,6 +15,14 @@ function App() {
 						<Route
 							path='/'
 							element={<Home />}
+						/>
+						<Route
+							path='/login'
+							element={<Login />}
+						/>
+						<Route
+							path='/signup'
+							element={<Signup />}
 						/>
 					</Routes>
 				</div>
