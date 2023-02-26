273d8c3f0236a01b03f1d610618f8f2eae3a3a77
jeffmikhail <mikhailcruz78@gmail.com>
2023-02-26 09:43:06 +0800

    logging users funcionality

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -7,7 +7,20 @@ const createToken = (_id) => {
 
 // * LOGIN
 const login = async (req, res) => {
-	res.json({ message: 'login user' });
+	const { email, password } = req.body;
+
+	try {
+		const user = await User.login(email, password);
+
+		// create a token
+		const token = createToken(user._id);
+
+		res.status(200).json({ email, token, user });
+	} catch (error) {
+		res.status(400).json({
+			error: error.message,
+		});
+	}
 };
 
 // * SIGNUP
